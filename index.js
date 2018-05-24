0a61bfe66bba5b6093a54d208d866628ce06d7f6
Mikhail Sivolobov <astronomer@gmail.com>
2018-05-24 20:16:13 +0700

    Added scripts for calculator buttons

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-var k = 3 > 2;
-console.log(k);
+var button1 = document.getElementById("button-1");
+var button2 = document.getElementById("button-2");
+var button3 = document.getElementById("button-3");
+var heading = document.getElementById("heading")
+
+changeColor = function (color) {
+    return function () {
+        heading.style.color = color;
+    }
+};
+
+increment = function (event) {
+    var i = parseInt(event.target.innerHTML);
+    event.target.innerHTML = i + 1;
+};
+
+buttons = document.getElementsByClassName("increment-button");
+for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click", increment);
+}
+
+button1.addEventListener("click", changeColor("red"));
+button2.addEventListener("click", changeColor("green"));
+button3.addEventListener("click", changeColor("blue"));
